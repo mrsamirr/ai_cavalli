26c2bd63d6946667e176e0cf15813abcd9c73f84
Md Samer Ansari <115770126+mrsamirr@users.noreply.github.com>
2026-02-17 23:50:23 +0530

    refactor: replace createClient with createBrowserClient for Supabase initialization

--- lib/database/supabase.ts
+++ lib/database/supabase.ts
@@ -1,11 +1,8 @@
+import { createBrowserClient } from "@supabase/ssr";
 
-import { createClient } from '@supabase/supabase-js'
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 
-export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-export const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.warn('Missing Supabase Environment Variables')
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
